ac8c545db0a12e08460e31385045c047dd39c13a
amitcoderhub <amitchauhan6599@gmail.com>
2024-09-27 12:29:11 +0530

    initial commit

--- program2.py
+++ program2.py
@@ -1,5 +1,24 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+    memo = {}
 
-# write your code here
-  
+    def match_helper(i, j):
+        if (i, j) in memo:
+            return memo[(i, j)]
+        
+        if j == len(p):
+            return i == len(s)
+        
+        if j < len(p) and p[j] == '*':
+            result = match_helper(i, j + 1) or (i < len(s) and match_helper(i + 1, j))
+            memo[(i, j)] = result
+            return result
+        
+        if i < len(s) and (p[j] == '?' or p[j] == s[i]):
+            result = match_helper(i + 1, j + 1)
+            memo[(i, j)] = result
+            return result
+        
+        memo[(i, j)] = False
         return False
+
+    return match_helper(0, 0)
